6cfbb02e5bea47eb6fca88f9be91ea59cb7808c6
asankhaperera <asankha.perera@gmail.com>
2020-03-25 18:57:24 +0530

    init

---
+++ PlayGround.js
@@ -0,0 +1,20 @@
+let AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
+
+exports.handler = async (event) => {
+
+    console.log("Hello world from one publish");
+    try {
+        let data = await sqs.sendMessage({
+            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/temp`,
+            MessageBody: "Hello World",
+            DelaySeconds: 0
+        }).promise();
+
+    } catch (err) {
+        console.log("Error " + err);
+    };
+
+
+    return { "message": "Successfully executed" };
+};
---
+++ another.js
@@ -0,0 +1,11 @@
+let AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
+
+exports.handler = async (event) => {
+
+    console.log("Hello world from the other receive");
+
+    console.log(JSON.stringify(event));
+
+    return { "message": "Successfully executed" };
+};
